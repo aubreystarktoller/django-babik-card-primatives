1bf6b3e3a3ba253faab90794ff9e8bec0ae99da4
Aubrey Stark-Toller <aubrey@deepearth.uk>
2016-03-16 16:47:24 +0000

    Added tests for validators

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,45 +1,97 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from babik_card_primitives.validators import CardNumberValidator
+from babik_card_primitives.validators import (
+    CardNumberLuhnTestValidator,
+    CardNumberIssuerWhitelistValidator,
+    CardSecurityCodeValidator
+)
 
 
-class CardNumberValidatorTestCase(TestCase):
+class CardNumberLuhnTestValidatorTestCase(TestCase):
     def test_nonsense_card_number(self):
-        validator = CardNumberValidator()
+        validator = CardNumberLuhnTestValidator()
         with self.assertRaises(ValidationError) as cm:
             validator("nonsense")
         self.assertEqual(cm.exception.code, "invalid_card_number")
 
     def test_nonsense_card_number_with_set_code(self):
-        validator = CardNumberValidator(code = "test")
+        validator = CardNumberLuhnTestValidator(code = "test")
         with self.assertRaises(ValidationError) as cm:
             validator("nonsense")
         self.assertEqual(cm.exception.code, "test")
 
     def test_nonsense_card_number_with_set_message(self):
-        validator = CardNumberValidator(message = "test")
+        validator = CardNumberLuhnTestValidator(message = "test")
         with self.assertRaises(ValidationError) as cm:
             validator("nonsense")
         self.assertEqual(cm.exception.message, "test")
 
     def test_invalid_card_number(self):
-        validator = CardNumberValidator()
+        validator = CardNumberLuhnTestValidator()
         with self.assertRaises(ValidationError) as cm:
             validator("49927398717")
         self.assertEqual(cm.exception.code, "invalid_card_number")
 
     def test_invalid_card_number_with_set_code(self):
-        validator = CardNumberValidator(code="test")
+        validator = CardNumberLuhnTestValidator(code="test")
         with self.assertRaises(ValidationError) as cm:
             validator("49927398717")
         self.assertEqual(cm.exception.code, "test")
 
     def test_invalid_card_number_with_set_message(self):
-        validator = CardNumberValidator(message="test")
+        validator = CardNumberLuhnTestValidator(message="test")
         with self.assertRaises(ValidationError) as cm:
             validator("49927398717")
         self.assertEqual(cm.exception.message, "test")
 
     def test_valid_card_number(self):
-        validator = CardNumberValidator()
+        validator = CardNumberLuhnTestValidator()
         validator("49927398716")
+
+
+class CardSecurityCodeValidatorTestCase(TestCase):
+    def test_valid_csc_length_3(self):
+        CardSecurityCodeValidator()("444")
+
+    def test_valid_csc_length_4(self):
+        CardSecurityCodeValidator()("4444")
+
+    def test_invalid_csc(self):
+        with self.assertRaises(ValidationError) as cm:
+            CardSecurityCodeValidator()("HAHAHA")
+        self.assertEqual(cm.exception.code, "invalid_csc")
+
+    def test_invalid_csc_with_set_code(self):
+        with self.assertRaises(ValidationError) as cm:
+            CardSecurityCodeValidator(code='test')("HAHAHA")
+        self.assertEqual(cm.exception.code, "test")
+
+    def test_invalid_csc_with_set_message(self):
+        with self.assertRaises(ValidationError) as cm:
+            CardSecurityCodeValidator(message="test")("HAHAHA")
+        self.assertEqual(cm.exception.message, "test")
+
+
+class CardNumberIssuerWhitelistValidatorTestCase(TestCase):
+    def test_valid(self):
+        CardNumberIssuerWhitelistValidator(["visa"])("4716492322141017")
+
+    def test_invalid(self):
+        with self.assertRaises(ValidationError) as cm:
+            CardNumberIssuerWhitelistValidator(["visa"])("5716492322141017")
+        self.assertEqual(cm.exception.code, "invalid_issuer")
+
+    def test_invalid_with_set_code(self):
+        with self.assertRaises(ValidationError) as cm:
+            CardNumberIssuerWhitelistValidator(["visa"], code="test")("5716492322141017")
+        self.assertEqual(cm.exception.code, "test")
+
+    def test_invalid_with_set_message(self):
+        with self.assertRaises(ValidationError) as cm:
+            CardNumberIssuerWhitelistValidator(["visa"], message="test")("5716492322141017")
+        self.assertEqual(cm.exception.message, "test")
+
+    def test_invalid_with_whitelist_miss(self):
+        with self.assertRaises(ValidationError) as cm:
+            CardNumberIssuerWhitelistValidator(["mastercard"])("4716492322141017")
+        self.assertEqual(cm.exception.code, "invalid_issuer")
