895119791c7c7cd2f6f1d882db0b7d9066a145f8
Aubrey Stark-Toller <aubrey@deepearth.uk>
2016-03-23 00:54:48 +0000

    Updated widget named, added test for client side widget

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from babik_card_primitives.widgets import (
     SensitiveTextInput,
     SensitiveSelect,
-    TwoFieldCardDateWidget
+    TwoFieldDateWidget
 )
 
 class SensitiveTextInputTestCase(TestCase):
@@ -142,7 +142,7 @@ class SensitiveSelectTestCase(TestCase):
         )
 
 
-class TwoFieldCardDateWidgetTestCase(TestCase):
+class TwoFieldDateWidgetTestCase(TestCase):
     maxDiff = None
 
     def _build_options(self, cs, selected=None):
@@ -168,7 +168,7 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
         return ''.join(options) 
 
     def test_render_with_default_options(self):
-        widget = TwoFieldCardDateWidget()
+        widget = TwoFieldDateWidget()
         output = widget.render("test", None)
 
         month_choices = [(n, n) for n in range(1,13)]
@@ -193,7 +193,7 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
         )
 
     def test_render_with_single_empty_value(self):
-        widget = TwoFieldCardDateWidget(empty_label = 'test')
+        widget = TwoFieldDateWidget(empty_label = 'test')
         output = widget.render("test", None)
 
         month_choices = [(n, n) for n in range(1,13)]
@@ -218,7 +218,7 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
         )
 
     def test_render_with_dict_empty_value(self):
-        widget = TwoFieldCardDateWidget(
+        widget = TwoFieldDateWidget(
             empty_label = {'month':'test_month', 'year':'test_year'}
         )
         output = widget.render("test", None)
@@ -246,11 +246,11 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
 
     def test_invalid_empty_value_with_missing_key(self):
         with self.assertRaises(ValueError):
-            widget = TwoFieldCardDateWidget(empty_label = {"month": 'x'})
+            widget = TwoFieldDateWidget(empty_label = {"month": 'x'})
 
     def test_invalid_empty_value_with_unknown_key(self):
         with self.assertRaises(ValueError):
-            widget = TwoFieldCardDateWidget(
+            widget = TwoFieldDateWidget(
                 empty_label = {
                     "month": "test_month",
                     "year": "test_year",
@@ -259,7 +259,7 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
             )
 
     def test_render_with_set_months(self):
-        widget = TwoFieldCardDateWidget(months = (1, 2))
+        widget = TwoFieldDateWidget(months = (1, 2))
         output = widget.render("test", None)
 
         month_choices = [(n, n) for n in (1, 2)]
@@ -284,7 +284,7 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
         )
 
     def test_render_with_set_years(self):
-        widget = TwoFieldCardDateWidget(years = (2016, 2017))
+        widget = TwoFieldDateWidget(years = (2016, 2017))
         output = widget.render("test", None)
 
         month_choices = [(n, n) for n in range(1,13)]
@@ -309,7 +309,7 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
 
     def test_render_with_non_empty_value(self):
         this_year = datetime.date.today().year
-        widget = TwoFieldCardDateWidget()
+        widget = TwoFieldDateWidget()
         output = widget.render("test", (1, this_year))
 
         month_choices = [(n, n) for n in range(1,13)]
@@ -334,7 +334,7 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
 
     def test_render_with_is_required(self):
         this_year = datetime.date.today().year
-        widget = TwoFieldCardDateWidget()
+        widget = TwoFieldDateWidget()
         widget.is_required = True
         output = widget.render("test", None)
 
@@ -360,7 +360,7 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
 
     def test_render_with_id(self):
         this_year = datetime.date.today().year
-        widget = TwoFieldCardDateWidget(attrs={'id':'myid'})
+        widget = TwoFieldDateWidget(attrs={'id':'myid'})
         widget.is_required = True
         output = widget.render("test", None)
 
@@ -385,7 +385,7 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
         )
 
     def test_value_from_datadict_with_zero_value(self):
-        widget = TwoFieldCardDateWidget()
+        widget = TwoFieldDateWidget()
         d = widget.value_from_datadict(
             {'test_month': "0", 'test_year': "0"},
             [],
@@ -394,15 +394,40 @@ class TwoFieldCardDateWidgetTestCase(TestCase):
         self.assertEqual(d, None)
 
     def test_value_from_datadict_with_set_value(self):
-        widget = TwoFieldCardDateWidget()
+        widget = TwoFieldDateWidget()
         d = widget.value_from_datadict(
             {'test_month': "1", 'test_year': "2016"},
             [],
             'test'
         )
-        self.assertEqual(d, ("1", "2016"))
+        self.assertEqual(d, {"month": "1", "year": "2016"})
 
     def test_value_from_datadict_with_partial_value(self):
-        widget = TwoFieldCardDateWidget()
+        widget = TwoFieldDateWidget()
         d = widget.value_from_datadict({'test_month': 'x'}, [], 'test')
         self.assertEqual(d, None)
+
+    def test_render_with_client_side_only(self):
+        widget = TwoFieldDateWidget(attrs={"client_side_only":True})
+        output = widget.render("test", None)
+
+        month_choices = [(n, n) for n in range(1,13)]
+        months = self._build_options([(0, '---')] + month_choices)
+        this_year = datetime.date.today().year
+        year_choices = [(n, n) for n in range(this_year,this_year+15)]
+        years = self._build_options([(0, '---')] + year_choices)
+        self.assertHTMLEqual(
+            output,
+            """
+            <div>
+            <select id="id_test_month">
+            {months}
+            </select>
+            </div>
+            <div>
+            <select id="id_test_year">
+            {years}
+            </select>
+            </div>
+            """.format(months=months, years=years)
+        )
