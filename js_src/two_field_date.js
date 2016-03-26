d380342652aeb394a8e7b6d952034465ec5618e4
Aubrey Stark-Toller <aubrey@deepearth.uk>
2016-03-26 14:44:57 +0000

    More work on str parser

--- js_src/two_field_date.js
+++ js_src/two_field_date.js
@@ -4,156 +4,198 @@ function TwoFieldDate(year, month) {
 }
 
 
-var strToTwoFieldDate = (function() {
-    var abbrMonths = [
-        "jan", "feb", "mar", "apr", "may", "jun",
-        "jul", "aug", "sep", "oct", "nov", "dec"
-    ]
-    var months = [
-        "january", "february", "march",
-        "april", "may", "june",
-        "july", "august", "september",
-        "october", "november", "december",
-    ]
+function TwoFieldDateStrParser(format) {
+    this.rawFormat = format;
 
-    var monthToInt = function(month) {
-        var temp = month.toLowerCase();
-        for (var i = 0; i < 12; i++) {
-            if (temp == months[i]) {
-                return i;
-            }
-        }
-        return null;
+    this.formatErrors = Array();
+    this.mapper = Array();
+
+    this.monthDirectiveFound = false;
+    this.yearDirectiveFound = false;
+
+    var inner = format.split("%%").map(this.buildRegex, this).join("%");
+
+    if (this.monthDirectiveFound && this.yearDirectiveFound) {
+        this.regex = RegExp("^" + inner + "$", "i");
+        this.mapperLength = mapper.length;
     }
-
-    var abbrMonthToInt = function(abbrMonth) {
-        var temp = abbrMonth.toLowerCase();
-        for (var i = 0; i < 12; i++) {
-            if (temp == abbrMonths[i]) {
-                return i;
-            }
-        }
-        return null;
+    else {
+        this.addFormatError(
+            'Both a year directive and a month directive must be specified'
+        );
     }
+}
+  
 
-    var directiveRegex = {
-        "b": "(" + abbrMonths.join("|") + ")",
-        "B": "(" + months.join("|") + ")",
-        "m": "([0 ]?[1-9]|1[0-2])",
-        "y": "([0-9]{2})",
-        "Y": "([0-9]{4})",
-    }
-    var directives = ["b", "B", "m", "y", "Y"];
-    var directivesLen = directives.length;
+TwoFieldDateStrParser.prototype.abbrMonths = [
+    "jan", "feb", "mar", "apr", "may", "jun",
+    "jul", "aug", "sep", "oct", "nov", "dec"
+]
 
-    var strayDirectiveRegex = RegExp("%(.)");
 
-    return function(str, format) {
-        var parts = format.split("%%");
-        var convertedParts = Array();
-        var len = parts.length;
-        var mapper = Array();
+TwoFieldDateStrParser.prototype.months = [
+    "january", "february", "march",
+    "april", "may", "june",
+    "july", "august", "september",
+    "october", "november", "december",
+]
 
-        for (var i = 0; i < len; i++) {
-            var temp = parts[i];
-            for (var j = 0; j < directivesLen; j++) {
-                var d = directives[j];
-                if (temp.includes("%" + d)) {
-                    temp = temp.replace("%" + d, directiveRegex[d]);
-                    mapper.append(d);
-                }
-            }
 
-            var match = temp.match(strayDirective);
-            if (match) {
-                ParseResult(
-                    null,
-                    ['"' + match[0] + '" is a bad directivr in format string "' + format + '"']
-                )
-            }
-            else {
-                convertedParts.push(temp);
-            }
-        }
-        
-        formatRegex = RegExp("^" + convertedParts.join("%") + "$");
-        var match = str.match(formatRegex);
-        if (match) {
-            var len = mapper.length;
-            var month = null;
-            var year = null;
-            for (var i = 0; i < len; i++) {
-                switch(mapper[i]) {
-                    case 'b':
-                        if (month !== null) {
-                            ParseResult(
-                                null,
-                                ['Mutltiple month directives are specified']
-                            )
-                        }
-                        month = abbrMonthToInt(match[i]);
-                        break;
-                    case 'B':
-                        if (month !== null) {
-                            ParseResult(
-                                null,
-                                ['Mutltiple month directives are specified']
-                            )
-                        }
-                        month = monthToInt(match[i]);
-                        break;
-                    case 'm':
-                        if (month !== null) {
-                            ParseResult(
-                                null,
-                                ['Mutltiple month directives are specified']
-                            )
-                        }
-                        month = parseInt(match[i]);
-                        break;
-                    case 'y':
-                        if (year !== null) {
-                            ParseResult(
-                                null,
-                                ['Mutltiple year directives are specified']
-                            )
-                        }
-                        var partialYear = parseInt(match[i]);
-                        if (partialYear < 69) {
-                            year = partialYear + 2000;
-                        }
-                        else {
-                            year = partialYear + 1900;
-                        }
-                        break;
-                    case 'Y':
-                        if (year !== null) {
-                            ParseResult(
-                                null,
-                                ['Mutltiple year directives are specified']
-                            )
-                        }
-                        year = parseInt(match[i]);
-                        break;
-                }
-                var data = match[i];
-            }
+TwoFielDateStrParser.prototype.directiveToRegex = {
+    "b": "(" + abbrMonths.join("|") + ")",
+    "B": "(" + months.join("|") + ")",
+    "m": "([0 ]?[1-9]|1[0-2])",
+    "y": "([0-9]{2})",
+    "Y": "([0-9]{4})",
+}
 
-            if (month !== null && year !== null) {
-                return TwoFieldDate(year, month);
-            }
-            else {
-                ParseResult(
-                    null,
-                    ['both a year and a month must be specified']
-                )
-            }
-        }
-        else {
-            ParseResult(
-                null,
-                ['string "' + str + '" does not match format string "' + format + '"']
-            )
+
+TwoFieldDateStrParser.prototype.directiveRegex = RegExp("%[bBmyY]");
+
+
+TwoFieldDateStrParser.prototype.strayDirectiveRegex = RegExp("%(.)");
+
+
+TwoFieldDateStrParser.prototype.monthToInt = function(month) {
+    var temp = month.toLowerCase();
+    for (var i = 0; i < 12; i++) {
+        if (temp == months[i]) {
+            return i;
         }
     }
-})();
+    return null;
+}
+
+
+TwoFieldDateStrParser.prototype.abbrMonthToInt = function(month) {
+    var temp = abbrMonth.toLowerCase();
+    for (var i = 0; i < 12; i++) {
+        if (temp == abbrMonths[i]) {
+            return i;
+        }
+    }
+    return null;
+}
+
+
+TwoFieldDateStrParser.prototype.addFormatError = function(error) {
+    this.formatErrors.push(error);
+}
+
+
+TwoFieldDateStrParser.prototype.addParseError = function(error) {
+    this.parseErrors.push(error);
+}
+
+
+TwoFieldDateStrParser.prototype.parseDirective = function(match, directive) {
+    this.mapper.push(directive);
+    if directive == 'b' || directive == 'B' || directive == 'm':
+        this.monthDirectiveFound = true;
+    else {
+        this.yaerDirectiveFound = true;
+    }
+    return this.directiveToRegex[directive];
+}
+
+
+TwoFieldDateStrParser.prototype.buildRegex = function(format) {
+    var temp = parts[i].replace(directiveRegex, parseDirective);
+    var match = temp.match(strayDirective);
+
+    if (match) {
+        this.addFormatError(
+            '"' + match[0] + '" is a bad directive in format string "' + this.rawFormat + '"'
+        )
+    }
+    else {
+        return temp;
+    }
+}
+
+
+TwoFieldDateStrParser.prototype.parse = function(str) {
+    this.parseErrors = Array();
+    var match = str.match(this.regex);
+
+    if (match) {
+        var month = null;
+        var year = null;
+
+        for (var i = 0; i < this.mapperLength; i++) {
+            switch(this.mapper[i]) {
+                case 'b':
+                    var temp = abbrMonthToInt(match[i]);
+                    if (month !== null && temp !== month) {
+                        this.addParseError(
+                            'Mutltiple, dfferent month values are given.'
+                        )
+                    }
+                    else {
+                        month = temp;
+                    }
+                    break;
+                case 'B':
+                    var temp = monthToInt(match[i]);
+                    if (month !== null && temp !== month) {
+                        this.addParseError(
+                            'Mutltiple, dfferent month values are given.'
+                        )
+                    }
+                    else {
+                        month = temp;
+                    }
+                    break;
+                case 'm':
+                    var temp = monthToInt(match[i]);
+                    if (month !== null && temp !== month) {
+                        this.addParseError(
+                            'Mutltiple, dfferent month values are given.'
+                        )
+                    }
+                    else {
+                        month = temp;
+                    }
+                    break;
+                case 'y':
+                    var partialYear = parseInt(match[i]);
+
+                    var temp;
+                    if (partialYear < 69) {
+                        temp = partialYear + 2000;
+                    }
+                    else {
+                        temp = partialYear + 1900;
+                    }
+
+                    if (year !== null && year !== temp) {
+                         this.addParseError(
+                            'Mutltiple, different year values are given'
+                         )
+                    }
+                    else {
+                        year = temp;
+                    }
+                    break;
+                case 'Y':
+                    var temp = parseInt(match[i]);
+                    if (year !== null && year !== temp) {
+                        this.addParseError(
+                            'Mutltiple, different year values are given'
+                        )
+                    }
+                    else {
+                        year = temp;
+                    }
+                    break;
+            }
+        }
+
+        return TwoFieldDate(year, month);
+    }
+    else {
+        this.addParseError(
+           'string "' + str + '" does not match format string "' + this.rawFormat + '"'
+        )
+    }
+}
